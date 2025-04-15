57d9c2171d06d59822027bae08e3d6a35d28c3ce
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-04-16 01:35:17 +0300

    Edited and tested save message api

--- backend/app/Http/Controllers/MessageController.php
+++ backend/app/Http/Controllers/MessageController.php
@@ -4,20 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Models\Message;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MessageController extends Controller
 {
-    public function save(Request $request) {
+    public function saveMessage(Request $request) 
+    {
         $request->validate([
-            'message' => 'required|string',
-            'user_id' => 'required|exists:users,id'
+            'message' => 'required|string'
         ]);
 
         $message = Message::create([
-            'user_id' => $request->user_id,
-            'message' => $request->message
+            'user_id' => Auth::id(),
+            'content' => $request->message
         ]);
 
-        return response()->json($message, 201);
+        return response()->json([
+            'message' => $message,
+            'user' => [
+                'id' => Auth::id(),
+                'name' => Auth::user()->name
+            ]
+        ], 201);
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MessageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,6 +10,7 @@ Route::group(["prefix" => "v0.1"], function() {
         Route::group(["prefix" => "user"], function() {
 
             Route::get('/validate-token', [AuthController::class, 'validateToken']);
+            Route::post('/save-message', [MessageController::class, 'saveMessage']);
 
         });
     }); 
